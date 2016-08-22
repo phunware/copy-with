225a0ded4e604ab7299991a1aac3f6c90a89f0d2
Dallon Feldner <dfeldner@phunware.com>
2016-08-22 17:05:42 -0500

    ESLint

--- src/copyWith.js
+++ src/copyWith.js
@@ -3,10 +3,9 @@ const objectAssign = require('object-assign');
 
 module.exports = function copyWith(base, changes) {
   // First check that any extension even needs to happen
-  let equal = true;
   for (const k of Object.keys(changes)) {
-    const v = changes[k];
     if (changes[k] !== base[k]) {
+      // There's at least one difference between objects.
       return objectAssign({}, base, changes);
     }
   }
