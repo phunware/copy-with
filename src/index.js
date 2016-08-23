9a78c60e971183b60feefbf0ed0aa9cc3a87ded8
Dallon Feldner <dfeldner@phunware.com>
2016-08-23 10:58:46 -0500

    Assert that all functions are exported

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 'use strict';
+
 exports.copyWith = require('./copyWith');
 exports.copyWithout = require('./copyWithout');
+exports.copyWithVal = require('./copyWithVal');
 exports.copyWithDeep = require('./copyWithDeep');
 exports.copyWithDeepVal = require('./copyWithDeepVal');
+exports.firstIfSame = require('./firstIfSame');
