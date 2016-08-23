00aa3445f991b07944e681120243efda52f7f450
Dallon Feldner <dfeldner@phunware.com>
2016-08-23 09:24:19 -0500

    Better way to implement copyWith

--- src/copyWith.js
+++ src/copyWith.js
@@ -2,14 +2,11 @@
 const objectAssign = require('object-assign');
 
 module.exports = function copyWith(base, changes) {
-  // First check that any extension even needs to happen
-  const changeKeys = Object.keys(changes);
-  for (var i = 0; i < changeKeys.length; i++) {
-    const k = changeKeys[i];
-    if (changes[k] !== base[k]) {
-      // There's at least one difference between objects.
-      return objectAssign({}, base, changes);
-    }
+  // First check that any changes even need to happen
+  const hasChanges = Object.keys(changes)
+    .some(k => changes[k] !== base[k]);
+  if (hasChanges) {
+    return objectAssign({}, base, changes);
   }
   return base;
 }
